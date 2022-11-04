13101ccdac09d924e8a13ecc3dbab63cf6901845
MarcelaRuiz <50890091+MarcelaRuiz@users.noreply.github.com>
2022-11-04 11:36:23 -0400

    Update streamlit_app.py
    add mojitos

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,7 @@ import streamlit
 streamlit.title('my parents healty dinner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-
+streamlit.text('🍌 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥝 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🍇Hard-Boiled Free-Range Egg')
 
